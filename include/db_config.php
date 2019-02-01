87396b8234312dbdd4edd1aaeeee12ca43476748
Paul <lirasan.paul@gmail.com>
2019-02-01 10:07:00 +0800

    db config change to fit prod server db

--- include/db_config.php
+++ include/db_config.php
@@ -3,7 +3,7 @@
 
 try {
 	//$options = array(PDO::ATTR_AUTOCOMMIT=>FALSE);
-	$pdo = new PDO('mysql:host=localhost;dbname=salon',"admin", "Welcone.1");
+	$pdo = new PDO('mysql:host=localhost;dbname=salon1',"Lucassadmin", "a48170806a");
 } catch (Exception $e) {
 	error_log($e->getMessage());
 	exit('Something weird happened'); //something a user can understand
--- include/funcs.php
+++ include/funcs.php
@@ -5,7 +5,7 @@
 function get_nextNo($branch_code) {
 //$branch_code = "01";
 
-	$pdo = new PDO('mysql:host=localhost;dbname=salon',"admin", "Welcone.1");
+	$pdo = new PDO('mysql:host=localhost;dbname=salon1',"Lucassadmin", "a48170806a");
     $next_no = $pdo->prepare("SELECT (COUNT(*) + 1) next_no FROM sales_hdr WHERE branch_cd = :branch_cd GROUP BY branch_cd");
     $next_no->execute(
     					array(':branch_cd' => $branch_code)
